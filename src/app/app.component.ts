a4f2e88f2869ee29b43484b4536cea06fc33f957
progr <progr@MAHMOUD>
2023-12-25 17:08:53 +0200

    Update Problem With Pixels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'HackloginForm';
+export class AppComponent implements OnInit{
+
+  Objects : any =[]
+  Total_Box = 0
+
+  Card_Width = 85
+  Card_Height = 85
+  ngOnInit(): void {
+    this.ChangeBoxCount()
+  }
+ChangeBoxCount(){
+  var Width :number = Math.round(window.innerWidth / this.Card_Width)
+  var Height :number = Math.round(window.innerHeight / this.Card_Height)
+
+  this.Total_Box = Width * Height
+  this.Objects=[]
+  for (let index = 0; index < this.Total_Box; index++) {
+    this.Objects.push({})
+  }
+}
+  onEnter(item){
+    item.Enter = true
+    item.Out = false
+
+  }
+  onLeave(item){
+    item.Enter = false
+    item.Out = true
+
+  }
 }
